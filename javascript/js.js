d303363616ff204d582a9951902375159a8cc622
mangcaugai1st <mangcaugai1st@DESKTOP-JC522F4>
2023-02-13 20:30:49 +0700

    2023/2/13

---
+++ javascript/js.js
@@ -0,0 +1,15 @@
+ function playVideo() {
+    document.getElementById("myVideo").play();
+}
+    function pauseVideo() {
+    document.getElementById("myVideo").pause();
+}
+    function video300() {
+    document.getElementById("myVideo").style.width = "300px";
+}
+    function video500() {
+    document.getElementById("myVideo").style.width = "500px";
+}
+    function video800() {
+    document.getElementById("myVideo").style.width = "800px";
+}
